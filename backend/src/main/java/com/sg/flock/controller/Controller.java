4e152f0cce1965d715ca886427d5612b66a8ff64
u <denizchildir88@gmail.com>
2023-12-27 16:38:28 -0800

    2

--- backend/src/main/java/com/sg/flock/controller/Controller.java
+++ backend/src/main/java/com/sg/flock/controller/Controller.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.*;
  * @author nicho
  */
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "80")
 @RestController
 @RequestMapping("/api")
 public class Controller {
